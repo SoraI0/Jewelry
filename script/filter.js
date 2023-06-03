abb406faa23973efd02b56932c94986e3bce7e90
SoraI0 <romamogir@gmail.com>
2023-06-03 16:26:45 +0300

    added data-atributes

---
+++ script/filter.js
@@ -0,0 +1,27 @@
+export class Filter {
+	constructor() {
+		// const $products = document.querySelectorAll('.main__products-product')
+		// const $checkboxes = document.querySelectorAll('.filter__group-list-item input')
+		// const $submitButton = document.querySelector('.filter__button')
+		// const $typeCheckbox = document.querySelectorAll('#type input')
+
+		// const arr = [1, 2, 3, 4, 5, 6]
+
+		// console.log($typeCheckbox.indexOf('braslets'));
+
+		// $submitButton.addEventListener('click', () => {
+		// 	$products.forEach(item => { item.style.display = 'none' })
+		// 	$products.forEach(item => {
+		// 		if ($typeCheckbox.indexOf(el => { item.getAttribute('data-type') && el.checked })) {
+		// 			item.style.display = 'flex'
+		// 		}
+		// 	})
+		// })
+
+		// function findFilter(filter) {
+		// 	return
+		// }
+
+
+	}
+}
--- script/index.js
+++ script/index.js
@@ -1,9 +1,11 @@
 import { Slider } from './slider.js'
 import { Menu } from "./menu.js";
 import { PageTransition } from "./page-transition.js"
+import { Filter } from "./filter.js"
 
 new Menu()
 new Slider()
 new PageTransition()
+new Filter()
 
 
