ad7628a77eb004672c640e4608a6ef816d52769e
spiritosantoo <allashlapak5@gmail.com>
2023-06-03 11:20:40 +0300

    alla 4commit

---
+++ script/index.js
@@ -0,0 +1,3 @@
+import { Menu } from "./menu.js";
+
+new Menu()
---
+++ script/menu.js
@@ -0,0 +1,25 @@
+export class Menu {
+   constructor() {
+      let menuBtn = document.querySelector('.header__menu-img')
+      let menuBtnCross = document.querySelector('.header__menu-img-cross')
+      let menu = document.querySelector('.header__menu')
+      let menuItem = document.querySelectorAll('.header__menu-item')
+      menuBtn.addEventListener('click', () => {
+         menu.classList.toggle('active');
+         menuBtnCross.style.display = 'block'
+      })
+      menuBtnCross.addEventListener('click', () => {
+         menu.classList.toggle('active');
+         menuBtnCross.style.display = 'none'
+      })
+      menuItem.forEach(element => {
+         element.addEventListener('click', () => {
+            menu.classList.toggle('active');
+            menuBtnCross.style.display = 'none'
+         })
+      })
+
+
+   }
+
+}
